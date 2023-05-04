627f00c6c2754db3cf7a8f5604475b4dbfd0fd09
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-05-04 20:54:14 -0300

    Adição dos objects das meets

--- src/meet/meet.module.ts
+++ src/meet/meet.module.ts
@@ -4,9 +4,13 @@ import { MeetController } from './meet.controller';
 import { UserModule } from 'src/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Meet, MeetSchema } from './schemas/meet.schema';
+import { MeetObject, MeetObjectSchema } from './schemas/meetobject.schema';
 
 @Module({
-  imports: [UserModule, MongooseModule.forFeature([{name: Meet.name, schema: MeetSchema}])],
+  imports: [UserModule, MongooseModule.forFeature([
+    {name: Meet.name, schema: MeetSchema}, 
+    {name: MeetObject.name, schema: MeetObjectSchema}
+  ])],
   providers: [MeetService],
   controllers: [MeetController],
   exports: [MongooseModule, MeetService]
