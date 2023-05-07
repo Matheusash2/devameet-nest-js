b4ac8eb5afdeec92f2fd0a6a75ab662ad7ddd306
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-05-07 11:43:20 -0300

    Criação da room

---
+++ src/room/helpers/roommessages.helper.ts
@@ -0,0 +1,7 @@
+export const RoomMessagesHelper = {
+    ROOM_LINK_NOT_FOUND: "Reunião não encontrada",
+    ROOM_MAX_USERS: "A quantidade máxima de usuários por sala é de 10 pessoas",
+    JOIN_USER_NOT_VALID: "Usuário informado inválido",
+    JOIN_LINK_NOT_VALID: "Link informado inválido",
+    MUTE_NOT_VALID: "Valor para mudo inválido",
+}
